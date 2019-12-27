9adf154f17ba56085ac3ad31acc3a3e375aed2ca
07451737136 <lucasmss16@gmail.com>
2019-12-27 17:10:43 -0300

    UPDATE

--- src/main/java/com/CRUD/estudo/config/ResourceServerConfig.java
+++ src/main/java/com/CRUD/estudo/config/ResourceServerConfig.java
@@ -1,6 +1,10 @@
 package com.CRUD.estudo.config;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -44,7 +49,15 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter{
 	                       .allowedHeaders("Content-Type", "Authorization");
 	    }
 	
-		
+	 @Component
+	 public class Scheduler {
+	    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+	 @Scheduled(fixedRate  = 5000)
+	    public void task() {
+	       System.out.println("Scheduler task with duration : " + sdf.format(new Date()));
+	       // throw new RuntimeException("Example to throw a runtime exception");
+	    }
+	 }
 	
 	}
 
